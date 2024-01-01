6fce111d274d88aeb5654ec6cee174972e5ccf54
Barrera Alan <129219461+Sartalan@users.noreply.github.com>
2024-01-01 20:55:03 -0300

    App/DiagramScreen => Chart Feature && Design | 0.9v

--- App/Indoor-React-Native/src/pages/DiagramScreen.jsx
+++ App/Indoor-React-Native/src/pages/DiagramScreen.jsx
@@ -2,17 +2,20 @@ import {Text, View, Image } from 'react-native'
 import {DiagramStyle} from '../styles/pagesStyles' 
 import {
   ProgressChart,
-  ContributionGraph,
-  LineChart
+  ContributionGraph
 } from "react-native-chart-kit";
 import { Dimensions } from "react-native";
 
 const screenWidth = Dimensions.get("window").width;
 const backgroundChart = '#121212'
+const watered = "#A3C7D6"
+const humidity = "#3F3B6C"
+const temperature = "#9F73AB"
 // each value represents a goal ring in Progress chart
 const data = {
-  labels: ["Regado", "Regado", "Grades"], // optional
-  data: [0.4, 0.6, 0.8]
+  labels: ["watered", "humidity", "temperature"], // optional
+  data: [1, 0.6, 0.8],
+  colors: [(watered), (humidity), (temperature)]
 };
 
 const commitsData = [
@@ -34,11 +37,10 @@ export function DiagramScreen() {
 
     <View style={DiagramStyle.container}>
       <View style={DiagramStyle.chart} >
-        <Text style={DiagramStyle.text}>Sensores</Text>
         <ProgressChart
           style={DiagramStyle.chartGraph}
           data={data}
-          width={screenWidth - 10}
+          width={screenWidth - 20}
           height={250}
           strokeWidth={10}
           radius={30}
@@ -49,20 +51,26 @@ export function DiagramScreen() {
             decimalPlaces: 2, // optional, defaults to 2dp
             color: (opacity = 1) => `rgba(250, 100, 255, ${opacity})`,
             labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            propsForLabels: {
+              fill: 'white',
+              fontSize: 20,
+              
+            }
           }}
-          hideLegend={false}
+          hideLegend={true}
+          withCustomBarColorFromData
         />
+          <Text style={DiagramStyle.text}>Sensores</Text>
       </View>
 
       <View style={DiagramStyle.chart}>
-      <Text style={DiagramStyle.text}>Dias de Riego</Text>
       <ContributionGraph
           style={DiagramStyle.chartGraph}
           values={commitsData}
           endDate={new Date("2024-03-01")}
           numDays={100}
-          width={screenWidth - 10}
-          height={220}
+          width={screenWidth - 20}
+          height={225}
           chartConfig={{
             backgroundGradientFrom: (backgroundChart),
             backgroundGradientTo: (backgroundChart),
@@ -71,6 +79,7 @@ export function DiagramScreen() {
             labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
           }}
         />
+          <Text style={DiagramStyle.text}>Dias de Riego</Text>
       </View>
     </View>
     
