b5a7633f8e58e26fa7c355df6d0444fa75430dce
Barrera Alan <129219461+Sartalan@users.noreply.github.com>
2024-01-31 13:58:24 -0300

    Program => Form Remake | v0.6

--- Program/Indoor-Tauri/src/App.jsx
+++ Program/Indoor-Tauri/src/App.jsx
@@ -3,21 +3,31 @@
 function App() {
 
   return (
-
     <>
-
-
-      <div className="top-bar"></div>
-      <form className="form-container">
-
-        <div className="top">
-          <Item />
+      <div className="container">
+        <div className="left-section">
+          <p className="top-text">Send a Push Notification!</p>
+          <img className="indoor-img" src="https://cdn-icons-png.flaticon.com/512/6565/6565893.png" alt="" />
+          <blockquote className="bottom-text">Indoor-App</blockquote>
         </div>
-      
-        <div className="bottom">
-          <button type="submit">Enviar</button>
+
+
+
+        <div className="right-section">
+          <div className="top-bar"></div>
+          <form className="form-container">
+
+            <div className="top">
+              <Item />
+            </div>
+
+            <div className="bottom">
+              <button type="submit">Enviar</button>
+            </div>
+          </form>
+
         </div>
-      </form>
+      </div>
     </>
   )
 }
@@ -29,6 +39,13 @@ function Item() {
     <div className="box">
       <label for="textoInput">Send a message</label>
       <textarea type="text" id="textoInput" name="textoInput" required />
+
+
+
+
     </div>
+
+
+
   )
 }
