a7ba6f44baaebaab6822523148715948ad1b6f05
Barrera Alan <129219461+Sartalan@users.noreply.github.com>
2024-02-23 21:27:52 -0300

    Docs => Refactored Code | v0.6

--- Docs/Indoor-Docs/src/components/HomepageFeatures/index.jsx
+++ Docs/Indoor-Docs/src/components/HomepageFeatures/index.jsx
@@ -1,101 +1,23 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import { FeatureListOne, FeatureListTwo, FeatureListThree } from './../HomepageIcons/FeatureIcons'
 
-
-const FeatureListOne = [
-  {
-    title: 'HTML5',
-    url: 'https://developer.mozilla.org/es/docs/Web/HTML',
-    Svg: require('@site/static/brand/html5-logo.svg').default,
-  },
-  {
-    title: 'CSS3',
-    url: 'https://developer.mozilla.org/es/docs/Web/CSS',
-    Svg: require('@site/static/brand/css3-logo.svg').default,
-  },
-  {
-    title: 'Javascript',
-    url: 'https://developer.mozilla.org/es/docs/Web/Javascript',
-    Svg: require('@site/static/brand/javascript-logo.svg').default,
-  },
-  {
-    title: 'React', 
-    url: 'https://developer.mozilla.org/es/docs/Web/Javascript',
-    Svg: require('@site/static/brand/react-logo.svg').default
-  },
-  {
-    title: 'Node.js',
-    url: 'https://developer.mozilla.org/es/docs/Web/Javascript',
-    Svg: require('@site/static/brand/nodejs-logo.svg').default,
-  },
-
-];
-/*?dwadwaawdd */
-const FeatureListTwo = [
-  {
-    title: 'Express',
-    url: 'https://developer.mozilla.org/es/docs/Web/Javascript',
-    Svg: require('@site/static/brand/express-logo.svg').default,
-  },
-  
-  {
-    title: 'Tauri',
-    url: 'https://developer.mozilla.org/es/docs/Web/Javascript',
-    Svg: require('@site/static/brand/tauri-logo.svg').default,
-  },
-
-  {
-    title: 'Rust',
-    url: 'https://developer.mozilla.org/es/docs/Web/Javascript',
-    Svg: require('@site/static/brand/rust-logo.svg').default,
-  },
-  {
-    title: 'C',
-    url: 'https://developer.mozilla.org/es/docs/Web/Javascript',
-    Svg: require('@site/static/brand/c-logo.svg').default,
-  },
-
-];
-
-const FeatureListThree = [
-  {
-    title: 'Git', 
-    url: 'https://developer.mozilla.org/es/docs/Web/Javascript',
-    Svg: require('@site/static/brand/git-logo.svg').default
-  },
-
-  {
-    title: 'Github', 
-    url: 'https://developer.mozilla.org/es/docs/Web/Javascript',
-    Svg: require('@site/static/brand/github-logo.svg').default
-  },
-
-
-  {
-    title: 'Raspberry Pi',
-    url: 'https://developer.mozilla.org/es/docs/Web/Javascript',
-    Svg: require('@site/static/brand/raspberrypi-logo.svg').default,
-  },
-]
-
-function Feature({Svg, title, url}) {
+function Feature({ Svg, title, url }) {
   return (
-      <div className="col text--center">
-        <a href={url} id={styles.techContainer}>
-          <Svg className={styles.techSvg} role="img"/>
-          <p className={styles.techText}>{title}</p>
-        </a>
-      </div>
+    <div className="col text--center">
+      <a href={url} target="_blank" id={styles.techA}>
+        <Svg className={styles.techIcon} role="img" />
+        <p className={styles.techText}>{title}</p>
+      </a>
+    </div>
   );
 }
 
 export default function HomepageFeatures() {
   return (
-    <section className={styles.features}>
+    <section className={styles.techSection}>
       <div className={styles.container}>
-      <h2 className={styles.techTitle}>Tecnologias Usadas:</h2>
-      <hr className={styles.hr} />
+        <h2 className={styles.techTitle}>Tecnologias Usadas:</h2>
+        <hr className={styles.hr} />
         <div className="row">
           {FeatureListOne.map((props, idx) => (
             <Feature key={idx} {...props} />
---
+++ Docs/Indoor-Docs/src/components/HomepageIcons/FeatureIcons.jsx
@@ -0,0 +1,81 @@
+export const FeatureListOne = [
+  {
+    title: 'HTML5',
+    url: 'https://developer.mozilla.org/es/docs/Web/HTML',
+    Svg: require('@site/static/brand/html5-logo.svg').default,
+  },
+  {
+    title: 'CSS3',
+    url: 'https://developer.mozilla.org/es/docs/Web/CSS',
+    Svg: require('@site/static/brand/css3-logo.svg').default,
+  },
+  {
+    title: 'Javascript',
+    url: 'https://developer.mozilla.org/es/docs/Web/Javascript',
+    Svg: require('@site/static/brand/javascript-logo.svg').default,
+  },
+  {
+    title: 'React', 
+    url: 'https://es.react.dev',
+    Svg: require('@site/static/brand/react-logo.svg').default
+  },
+  {
+    title: 'Node.js',
+    url: 'https://nodejs.org/en',
+    Svg: require('@site/static/brand/nodejs-logo.svg').default,
+  },
+
+];
+
+export const FeatureListTwo = [
+  {
+    title: 'Express.js',
+    url: 'https://expressjs.com/es/',
+    Svg: require('@site/static/brand/express-logo.svg').default,
+  },
+  
+  {
+    title: 'Tauri',
+    url: 'https://tauri.app',
+    Svg: require('@site/static/brand/tauri-logo.svg').default,
+  },
+
+  {
+    title: 'Tailwind.css',
+    url: 'https://tailwindcss.com',
+    Svg: require('@site/static/brand/tailwind-logo.svg').default,
+  },
+
+  {
+    title: 'Rust',
+    url: 'https://www.rust-lang.org/es',
+    Svg: require('@site/static/brand/rust-logo.svg').default,
+  },
+  {
+    title: 'C',
+    url: 'https://es.wikipedia.org/wiki/C_(lenguaje_de_programación)',
+    Svg: require('@site/static/brand/c-logo.svg').default,
+  },
+
+];
+
+export const FeatureListThree = [
+  {
+    title: 'Git', 
+    url: 'https://git-scm.com',
+    Svg: require('@site/static/brand/git-logo.svg').default
+  },
+
+  {
+    title: 'Github', 
+    url: 'https://github.com',
+    Svg: require('@site/static/brand/github-logo.svg').default
+  },
+
+
+  {
+    title: 'Raspberry Pi',
+    url: 'https://www.raspberrypi.com',
+    Svg: require('@site/static/brand/raspberrypi-logo.svg').default,
+  },
+]
